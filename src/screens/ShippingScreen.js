551a534c27011fe155f474d2623491112536cb43
khr-rakib <khr.rakib@gmail.com>
2021-05-16 20:51:07 +0600

    checkout process done

--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
 import { saveShippingAddress } from '../actions/cartActions';
